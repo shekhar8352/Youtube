a759754db1837983643bdc09f62f61125c646954
sudhanshu8352 <82777068+sudhanshu8352@users.noreply.github.com>
2024-02-18 18:48:40 +0530

    Utils done and app created

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+dotenv.config()
+
+console.log(process.env.DATABASE_URL);
+
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running on port: ${process.env.PORT}`);
+  })
+})
+.catch((err) => {
+  console.log("Connection failed", err);
+})
